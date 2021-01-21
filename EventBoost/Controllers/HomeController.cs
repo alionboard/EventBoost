2565c22b3548e1c6b37f8568bf1a460f60f9f53b
alionboard <alimustafaozbek@gmail.com>
2021-01-21 09:30:35 +0300

    Third commit

--- EventBoost/Controllers/HomeController.cs
+++ EventBoost/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using EventBoost.Models;
+﻿using EventBoost.Data;
+using EventBoost.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -12,15 +13,18 @@ namespace EventBoost.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
+            _db = applicationDbContext;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var meetings = _db.Meetings.OrderBy(x => x.MeetingTime).ToList();
+            return View(meetings);
         }
 
         public IActionResult Privacy()
