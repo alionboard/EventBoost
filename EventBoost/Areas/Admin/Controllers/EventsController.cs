f067bb8d2c8a2080cee74f81ef9d566a0692724f
AliOzbek <alimustafaozbek@gmail.com>
2021-01-26 09:26:29 +0300

    Edit event

--- EventBoost/Areas/Admin/Controllers/EventsController.cs
+++ EventBoost/Areas/Admin/Controllers/EventsController.cs
@@ -37,11 +37,11 @@ namespace EventBoost.Areas.Admin.Controllers
 
             if (ModelState.IsValid)
             {
-            string fileName = null;
+                string fileName = null;
                 if (vm.Photo != null && vm.Photo.Length > 0)
                 {
                     fileName = vm.Photo.GenerateFileName();
-                    var savePath = Path.Combine(env.WebRootPath,"img",fileName);
+                    var savePath = Path.Combine(env.WebRootPath, "img", fileName);
                     vm.Photo.CopyTo(new FileStream(savePath, FileMode.Create));
                 }
                 var meeting = new Meeting()
@@ -50,12 +50,63 @@ namespace EventBoost.Areas.Admin.Controllers
                     Description = vm.Description,
                     MeetingTime = vm.MeetingTime,
                     Place = vm.Place,
-                    PhotoPath=fileName
+                    PhotoPath = fileName
                 };
                 _db.Add(meeting);
                 _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Meeting meeting = _db.Meetings.Find(id);
+
+            if (meeting == null)
+            {
+                return NotFound();
             }
 
+            var vm = new EditMeetingViewModel()
+            {
+                Id = meeting.Id,
+                Title = meeting.Title,
+                Description = meeting.Description,
+                MeetingTime = meeting.MeetingTime,
+                ExistingPhotoPath = meeting.PhotoPath,
+                Place = meeting.Place
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditMeetingViewModel vm, [FromServices] IWebHostEnvironment env)
+        {
+            if (ModelState.IsValid)
+            {
+                string fileName = null;
+                if (vm.Photo != null && vm.Photo.Length > 0)
+                {
+                    fileName = vm.Photo.GenerateFileName();
+                    var savePath = Path.Combine(env.WebRootPath, "img", fileName);
+                    vm.Photo.CopyTo(new FileStream(savePath, FileMode.Create));
+                }
+                var meeting = _db.Meetings.Find(vm.Id);
+                meeting.MeetingTime = vm.MeetingTime;
+                meeting.Description = vm.Description;
+                meeting.Place = vm.Place;
+                meeting.Title = vm.Title;
+                if (!string.IsNullOrEmpty(fileName))
+                {
+                    //todo: mevcut resim varsa sil
+                    meeting.PhotoPath = fileName;
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
