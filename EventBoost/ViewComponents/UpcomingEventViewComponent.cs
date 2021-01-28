f2655995f66381b9bcaf6f70c7562f2858a8307f
Chester <alimustafaozbek@gmail.com>
2021-01-28 13:10:57 +0300

    ViewComponent Added

---
+++ EventBoost/ViewComponents/UpcomingEventViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using EventBoost.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventBoost.ViewComponents
+{
+    public class UpcomingEventViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UpcomingEventViewComponent(ApplicationDbContext dbContext)
+        {
+            _db = dbContext;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var meeting = await _db.Meetings
+                .Where(x => x.MeetingTime > DateTime.Now)
+                .OrderBy(x => x.MeetingTime)
+                .FirstOrDefaultAsync();
+
+            return View(meeting);
+        }
+    }
+}
